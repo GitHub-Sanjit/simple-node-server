7cd97ce6f6fff2520955486fb9f6d0e14fd33187
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-30 00:23:14 +0600

    Create mongodb atlas account and Connect to database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -17,10 +18,30 @@ const users = [
   { id: 4, name: "Sabana4", email: "Sabana4@gmail.com" },
 ];
 
+// Mongo DB Code Start
+
+const uri =
+  "mongodb+srv://dbUser1:123456sanjit@cluster0.zevqp.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("simpleNode").collection("users");
+  // perform actions on the collection object
+  console.log("database connected");
+  client.close();
+});
+
+// Mongo DB Code End
+
 app.get("/users", (req, res) => {
   if (req.query.name) {
     const search = req.query.name;
-    const filtered = users.filter((usr) => usr.name.toLowerCase().indexOf(search) >= 0);
+    const filtered = users.filter(
+      (usr) => usr.name.toLowerCase().indexOf(search) >= 0
+    );
     res.send(filtered);
   } else {
     res.send(users);
