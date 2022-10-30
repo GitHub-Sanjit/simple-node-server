a7c7d8b8d3ed5f0ebfae7e17c490ce3e6b33d047
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-30 10:38:55 +0600

    64-8 Module summary and find operation to load data

--- index.js
+++ index.js
@@ -31,15 +31,22 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const userCollection = client.db("simpleNode").collection("users");
-    const user = { name: "Baby Sarkar", email: "baby@gmail.com" };
+    // const user = { name: "Baby Sarkar", email: "baby@gmail.com" };
     // const result = await userCollection.insertOne(user);
     // console.log(result);
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find({});
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
     app.post("/users", async (req, res) => {
       console.log("Post API Called");
       const user = req.body;
       const result = await userCollection.insertOne(user);
       console.log(result);
-      user.id = result.insertedId;
+      user._id = result.insertedId;
 
       res.send(user);
     });
@@ -52,17 +59,17 @@ run().catch(console.dir);
 
 // Mongo DB Code End
 
-app.get("/users", (req, res) => {
-  if (req.query.name) {
-    const search = req.query.name;
-    const filtered = users.filter(
-      (usr) => usr.name.toLowerCase().indexOf(search) >= 0
-    );
-    res.send(filtered);
-  } else {
-    res.send(users);
-  }
-});
+// app.get("/users", (req, res) => {
+//   if (req.query.name) {
+//     const search = req.query.name;
+//     const filtered = users.filter(
+//       (usr) => usr.name.toLowerCase().indexOf(search) >= 0
+//     );
+//     res.send(filtered);
+//   } else {
+//     res.send(users);
+//   }
+// });
 
 // app.post("/users", (req, res) => {
 //   console.log("Post API Called");
